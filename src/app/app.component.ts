11165d536f20f2d403a876d0ca264e7ce63823c0
Christian Drochon <khrisys@protonmail.com>
2020-07-18 12:16:33 +0200

    Creation des type de var devant peupler le composant Post. Intégration du service dans le composant principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from '../services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angularBlog';
+
+  post: {
+    title: string,
+    content: string,
+    loveIts: number,
+    created_at: Date
+  };
+
+  posts: any[];
+
+  /**
+   * Declare le service
+   * @param postService
+   */
+  constructor(private postService: PostService) {
+  }
+
+  /**
+   * Initialise le service
+   */
+  ngOnInit(): void {
+    this.posts = this.postService.posts;
+  }
+
 }
