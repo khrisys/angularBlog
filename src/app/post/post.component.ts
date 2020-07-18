14fd07cf0ba6815fc59112e1006fc478eb9395a0
Christian Drochon <khrisys@protonmail.com>
2020-07-18 12:21:17 +0200

    Creation du composant Post au niveau html, sachant qu'il doit s'integrer dans le composant principal, donc Attention aux class bootstrap qui doivent se suivre correctement entre les balises des 2 composants.
    Declaration des var avec @Input()
    
    Declaration des methodes dont on a besoin pour les likes. Ces methodes font appel aux methodes declarées dans le service "postService".
    
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {PostService} from '../../services/post.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postDate: Date;
+
+  @Input() indexOfPst: number;
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  // ===============================================================
+  // Methodes personalisées uniquement destinées au composant "Post"
+  // ===============================================================
+  onLove() {
+    console.log('onLove');
+    this.postService.onLove(this.indexOfPst);
+  }
+
+  onHate() {
+    console.log('onHate');
+    this.postService.onHate(this.indexOfPst);
+  }
+}
