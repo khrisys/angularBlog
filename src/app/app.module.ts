ac12749efa0279b1c650601160ba6fae83cb05ae
Christian Drochon <khrisys@protonmail.com>
2020-07-25 19:14:04 +0200

    Ajout du composant error404, avec ajout dans le path et redirection des pages non trouvées (des urls inconnus pour l'appli)
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {PostFrameComponent} from './post-frame/post-frame.component';
 import {PostFrameInComponent} from './post-frame-in/post-frame-in.component';
 import {PostDetailComponent} from './post-detail/post-detail.component';
 import {PostNewComponent} from './post-new/post-new.component';
+import {Error404Component} from './error404/error404.component';
 
 // Attention à bien respecter l'ordre de declaration des paths'
 const appRoutes: Routes = [
@@ -16,7 +17,11 @@ const appRoutes: Routes = [
   {path: 'new', component: PostNewComponent},
   {path: ':id', component: PostDetailComponent},
   {path: 'posts/:id', component: PostDetailComponent},
-  {path: '', component: PostFrameComponent}
+  {path: 'not-found', component: Error404Component},
+  {path: '', component: PostFrameComponent},
+  {path: '**', redirectTo: '/not-found'} // path wildcard : redirection vers la page 404. IL EST ESSENTIEL DE METTRE LE PATH WILDCARD A
+  // LA FIN DES PATH, car Angular regarde les path dans l'rodre de declaration. Si le wildcard est au milieu du tab de path, si Angular
+  // tombe sur le wildcard, n'importe quelle route correspond au wildcard, et donc, tous les urls rentrés vont correspondre à ce wildc
 ];
 
 @NgModule({
@@ -35,7 +40,7 @@ const appRoutes: Routes = [
   providers: [
     PostService
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
