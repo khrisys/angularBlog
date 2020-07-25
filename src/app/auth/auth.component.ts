50d70a28a3ce9a130240a7c40f5f229916e7e506
Christian Drochon <khrisys@protonmail.com>
2020-07-25 19:26:11 +0200

    Creation et declaration du composant pour l'authentification à l'appli. Ajout des methodes SignIn et SignOUt ui correspondent au service auth.
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../services/auth-service';
+
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+
+  authStatus: boolean;
+
+  /**
+   * Constructeur de l'objet Auth.
+   * Injection de authService et Router
+   *
+   * @param authService : service d'authentification
+   * @param router : permet la redirection vers une page precise apres authentification
+   */
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
+  }
+
+  /**
+   * Comme la methode signIn est async, on utilise le "then()" pour prendre en charge le coté async
+   * Le router permet de naviguer vers un ou plusieurs composants precis lors de l'authentification (ici, on va vers le composant des
+   * utilisateurs)
+   *
+   * Dans le callback de cette methode,  on redirige l'user vers la page des utilisateurs grace à "navigate()" auquel on passe un array.
+   */
+  onSignIn() {
+    this.authService.signIn().then(() => {
+      // console.log('connecté !');
+      this.authStatus = this.authService.isAuth;
+      this.router.navigate(['users']);
+    });
+  }
+
+  onSignOut() {
+    this.authService.signOut();
+    this.authStatus = this.authService.isAuth;
+  }
+}
