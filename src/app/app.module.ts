2e99e54ae419d44a5c17cfe12418d83ffd861be5
Christian Drochon <khrisys@protonmail.com>
2020-07-25 19:41:30 +0200

    Creation du service auth-guard permettant de rediriger automatiquement un user qui saisirait une url non reconnues dans l'appli
    Reommage du service auth. Je m'etais trompé, je l'avais nommé 'auth-service.ts' au lieu de 'auth.service.ts'. Il y a un '.' au lieu d'un '-' entre auth et service.
    
    Mise en place des guards sur les path dans le composant principal
    Ajout des composants et des services créés dans le composant principal
    
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,19 @@ import {PostFrameInComponent} from './post-frame-in/post-frame-in.component';
 import {PostDetailComponent} from './post-detail/post-detail.component';
 import {PostNewComponent} from './post-new/post-new.component';
 import {Error404Component} from './error404/error404.component';
-import {AuthService} from './services/auth-service';
+import {AuthService} from './services/auth.service';
 import {AuthComponent} from './auth/auth.component';
+import {AuthGuardService} from './services/auth-guard';
 
 // Attention à bien respecter l'ordre de declaration des paths'
 const appRoutes: Routes = [
-  {path: 'posts', component: PostFrameComponent},
+  {path: 'posts', canActivate: [AuthGuardService], component: PostFrameComponent},
   {path: 'auth', component: AuthComponent},
-  {path: 'new', component: PostNewComponent},
-  {path: ':id', component: PostDetailComponent},
-  {path: 'posts/:id', component: PostDetailComponent},
+  {path: 'new', canActivate: [AuthGuardService], component: PostNewComponent},
+  {path: ':id', canActivate: [AuthGuardService], component: PostDetailComponent},
+  {path: 'posts/:id', canActivate: [AuthGuardService], component: PostDetailComponent},
   {path: 'not-found', component: Error404Component},
-  {path: '', component: PostFrameComponent},
+  {path: '', canActivate: [AuthGuardService], component: PostFrameComponent},
   {path: '**', redirectTo: '/not-found'} // path wildcard : redirection vers la page 404. IL EST ESSENTIEL DE METTRE LE PATH WILDCARD A
   // LA FIN DES PATH, car Angular regarde les path dans l'rodre de declaration. Si le wildcard est au milieu du tab de path, si Angular
   // tombe sur le wildcard, n'importe quelle route correspond au wildcard, et donc, tous les urls rentrés vont correspondre à ce wildc
@@ -33,7 +34,9 @@ const appRoutes: Routes = [
     PostFrameInComponent,
     PostFrameComponent,
     PostDetailComponent,
-    PostNewComponent
+    PostNewComponent,
+    AuthComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     PostService,
-    AuthService
+    AuthService,
+    AuthGuardService
   ],
   bootstrap: [AppComponent]
 })
