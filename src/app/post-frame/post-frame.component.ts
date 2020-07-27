a3e9f17056d7efdafeb4c4178134ce3fcb872591
Christian Drochon <khrisys@protonmail.com>
2020-07-27 10:36:44 +0200

    Ajout de la methode de destruction de l'observable du Subject
    Signed-off-by: Christian Drochon <khrisys@protonmail.com>

--- src/app/post-frame/post-frame.component.ts
+++ src/app/post-frame/post-frame.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostService} from '../services/post.service';
 import {Subscription} from 'rxjs';
 
@@ -7,14 +7,23 @@ import {Subscription} from 'rxjs';
   templateUrl: './post-frame.component.html',
   styleUrls: ['./post-frame.component.css']
 })
-export class PostFrameComponent implements OnInit {
+export class PostFrameComponent implements OnInit, OnDestroy {
 
   posts: any[];
+  // Il faut importer Observable, mais aussi
+  //    * 'rxjs/Rx', et surtout, avec les MAJ qu'il y a eu dans RxJS, il faut installer le package 'rxjs-compat' pour
+  //    * pouvoir utiliser cette methode 'interval'
   postSubscription: Subscription;
 
   constructor(private postService: PostService) {
   }
 
+  /**
+   * Executé au moment de la creation du component, mais apres l'execution du constructor
+   *
+   * Pour eviter tout pb lorsqu'on utilise des Observable perso, il est vivement conseillé d'utiliser la souscription via l'objet
+   * Subscription
+   */
   ngOnInit(): void {
     // creer la subscritpion
     this.postSubscription = this.postService.postSubject.subscribe((posts: any[]) => {
@@ -22,6 +31,14 @@ export class PostFrameComponent implements OnInit {
     });
     // emettre le subject
     this.postService.emitPostSubject();
+  }
+
+  /**
+   * Methode permettant de detruire la souscription (grace à 'unsubscribe()') à la fin de vie du component, et empeche ainsi les
+   * comportements innatendus liés aux Observable infinis
+   */
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
   }
 
 }
